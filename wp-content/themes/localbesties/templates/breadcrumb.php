0e8d1d404319af010127338980c6a742c8707733
Minh <minhvu2904@gmail.com>
2024-11-04 23:40:54 +0700

    update

--- wp-content/themes/localbesties/templates/breadcrumb.php
+++ wp-content/themes/localbesties/templates/breadcrumb.php
@@ -1,15 +1,18 @@
-<!--<div class="box-breadcrumb">-->
-<!--    --><?php //echo get_the_category_list() ?>
-<!--</div>-->
 <?php
 $categories = get_the_category();
+$terms = get_the_terms(get_the_ID(), 'location');
 ?>
 <div class="box-breadcrumb-tags">
     <div class="list-tag">
-        <a href="article.html">HaNoi</a>
-        <?php if ( ! empty( $categories ) ) : ?>
-        <a href="<?php echo esc_url( get_category_link( $categories[0]->term_id ) ) ?>"><?php echo esc_html( $categories[0]->name )?></a>
+        <?php if (isset($parent_category)): ?>
+            <a href="<?php echo get_url_category($parent_category->slug) . '?location=' . $terms[0]->slug ?>"><?php echo $terms[0]->name ?></a>
         <?php endif; ?>
+        <?php if (!empty($categories)) :
+            foreach ($categories as $category) :?>
+                <a href="<?php echo esc_url(get_category_link($category->term_id)) ?>"><?php echo esc_html($category->name) ?></a>
+            <?php
+            endforeach;
+        endif; ?>
     </div>
     <div class="tag-line"></div>
 </div>
