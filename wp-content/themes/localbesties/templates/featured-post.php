176927bf5fe14a6683adbc0ebbbcc5dbccd8d77e
DoanTV <doantvsp@gmail.com>
2024-12-05 04:32:35 +0700

    fix page home

--- wp-content/themes/localbesties/templates/featured-post.php
+++ wp-content/themes/localbesties/templates/featured-post.php
@@ -9,7 +9,7 @@
                     <img src="<?= get_the_post_thumbnail_url($featured_post->ID) ?>" />
                 </a>
                 <div class="box-fea-main">
-                    <div class="box-auth-time"><strong><?= $featured_post->post_author ?></strong> . <?= get_the_time('F d, Y', $featured_post->ID); ?></div>
+                    <div class="box-auth-time"><strong><?= get_author_name($featured_post->post_author); ?></strong> . <?= get_the_time('F d, Y', $featured_post->ID); ?></div>
                     <h3><a href="blog_detail.html"><?= $featured_post->post_title ?></a></h3>
                 </div>
             </div>
